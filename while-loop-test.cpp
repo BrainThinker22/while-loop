904688f3201b47be2090fb30ff63de8743a433b9
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 11:21:55 +0200

    Add files via upload

---
+++ while-loop-test.cpp
@@ -0,0 +1,91 @@
+/*
+
+        // SIMPLE WHILE LOOP CODE WITH INSERTED METHOD IN IT
+
+    #include <iostream>
+    #include <string>
+
+
+    int main()
+{
+
+    std::string name;
+
+    while (name.empty()) {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // WHILE LOOP NUMBER CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number = 0;
+
+    while (number <= 0) {
+        std::cout << "Enter a positive number: ";
+        std::cin >> number;
+    }
+
+        std::cout << "The written number is: " << number;
+
+        return 0;
+
+}
+
+*/
+
+/*
+
+        // WHILE LOOP NUMBERS FROM 1 TO 5
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        std::cout << i << " ";
+        ++i;
+    }
+
+    return 0;
+}
+
+*/
+
+/*
+
+        // DO WHILE LOOP CODE
+
+    #include <iostream>
+    #include <string>
+
+    int main() {
+
+    int number;
+
+    do {
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+    }
+        while (number <= 0);
+
+    std::cout << "The written number is: " << number;
+
+    return 0;
+}
+
+*/
+
