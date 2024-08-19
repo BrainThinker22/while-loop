91068e6a857f8211feebd93ccc30ec0e56523aa4
BrainThinker22 <gmpreslavcho@gmail.com>
2024-08-19 11:35:30 +0200

    Add files via upload

---
+++ while-loop-one-to-five.cpp
@@ -0,0 +1,15 @@
+// WHILE LOOP NUMBERS FROM 1 TO 5
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        std::cout << i << " ";
+        ++i;
+    }
+
+    return 0;
+}
